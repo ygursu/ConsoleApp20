c878b031709d446e0af0a62dc0e44497bfc670c2
ygursu <yasemingrs@gmail.com>
2024-01-20 20:22:29 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,23 @@
+﻿{
+    int sayi, hak = 5;
+    bool durum = false;
+    Random rastgele = new Random();
+    int rastgeleSayi = rastgele.Next(0, 100);
+    for (int i = hak; i > 0; i--)
+    {
+        Console.WriteLine("Hak {0}", i);
+        Console.Write("Tahmininiz :");
+        sayi = int.Parse(Console.ReadLine());
+        if (sayi == rastgeleSayi)
+        {
+            Console.WriteLine("Tebrikler");
+            durum = true;
+            break;
+        }
+    }
+    if (!durum)
+    {
+        Console.WriteLine("Tekrar Deneyin!");
+        Console.WriteLine("Sayı : {0}", rastgeleSayi);
+    }
+    Console.ReadKey();
